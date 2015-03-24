827e1a57b6c43f3d13a42dd2fcf8300073da85e6
Johnny Domino <johnny@yayuhh.com>
2015-03-24 10:14:31 -0400

    add google, facebook and github login

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,14 +1,52 @@
 import Ember from 'ember';
+import ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(ApplicationRouteMixin, {
   actions: {
-    login: function() {
-      this.get('session').authenticate('simple-auth-authenticator:torii', 'firebase', {
-        authWith: 'twitter'
-      });
+    login: function(provider) {
+      return this.get('session')
+        .authenticate('simple-auth-authenticator:torii', 'firebase', {
+          authWith: provider
+        });
     },
+
     logout: function() {
-      this.get('session').invalidate();
+      return this.get('session').invalidate();
+    },
+
+    sessionAuthenticationSucceeded: function() {
+      let store = this.get('store');
+      let uid   = this.get('session.uid');
+
+      return store.find('user', uid)
+        .then((/*user*/) => {
+
+        })
+        .catch(() => {
+          let user = store.createRecord('user', {
+            id: uid,
+            handle: this._handleFor(this.get('session.content'))
+          });
+
+          user.save().then((/*user*/) => {
+
+          });
+        });
+    }
+  },
+
+
+  _handleFor: function(authorization) {
+    if (authorization.github) {
+      return authorization.github.username;
+    } else if (authorization.facebook) {
+      return authorization.facebook.displayName;
+    } else if (authorization.twitter) {
+      return authorization.twitter.displayName;
+    } else if (authorization.google) {
+      return authorization.google.displayName;
+    } else {
+      throw new Error('couldn\'t find a username!');
     }
   }
 });
